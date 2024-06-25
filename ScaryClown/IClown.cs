053a743896d96d5dd3cbad8de31b7f6d5093bc41
Bartek <bartlomiej.kacki@gmail.com>
2024-06-25 20:28:54 +0200

    Dodano metodę ScareAdults z implementacją domyślną

--- ScaryClown/IClown.cs
+++ ScaryClown/IClown.cs
@@ -10,11 +10,37 @@ namespace ScaryClown
     {
         string FunnyThingIHave { get; }
         void Honk();
+
+        protected static Random random = new Random();
+        private static int carCapacity = 12;
+        public static int CarCapacity
+        {
+            get { return carCapacity; }
+            set
+            {
+                if (value > 10) carCapacity = value;
+                else Console.Error.WriteLine($"Uwaga! Pojemność autka {value} jest za mała!");
+            }
+        }
+
+        public static string ClownCarDescription()
+        {
+            return $"Liczba klaunów w autku: {random.Next(CarCapacity / 2, CarCapacity)}.";
+        }
     }
 
     internal interface IScaryClown : IClown
     {
         string ScaryThingIHave { get; }
         void ScareLittleChildren();
+
+        void ScareAdults()
+        {
+            Console.WriteLine(
+                $@"Jestem pradawnym złem prześladującym Cię we snach.
+Spójrz na mój przerażający naszyjnik z {random.Next(4,10)} palcami moich ofiar.
+A, byłbym zapomniał...");
+            ScareLittleChildren();
+        }
     }
 }
--- ScaryClown/Program.cs
+++ ScaryClown/Program.cs
@@ -7,7 +7,12 @@
             IClown fingersTheClown = new ScaryScary("wielkie czerwone nosy", 14);
             fingersTheClown.Honk();
             IScaryClown iScaryClownReference = fingersTheClown as IScaryClown;
-            iScaryClownReference.ScareLittleChildren();
+            iScaryClownReference.ScareAdults();
+
+            Console.WriteLine();
+
+            IClown.CarCapacity = 18;
+            Console.WriteLine(IClown.ClownCarDescription());
         }
     }
 }
